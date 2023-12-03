64aa540f0ad81a5a2ca2a40ce75ae21950bce1b8
Oleksii <example@gmail.com>
2023-12-03 11:51:53 +0100

    add personalMovieDB

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log(1);
+const numberOfFilms = +prompt ('How many filmes you allready watched?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('One of the latest films what you seen?', ''),
+      b = prompt('How you evaluate them?', ''),
+      c = prompt('One of the latest films what you seen?', ''),
+      d = prompt('How you evaluate them?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
